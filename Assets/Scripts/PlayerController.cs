198651483a54d20774d378fbaf47b3fb1eba916c
Mnkach <mnkach1@yandex.ua>
2016-02-17 01:41:37 +0200

    Fixed jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float playerSpeed;
+	public float jumpForce;
 
 	private Rigidbody2D rb;
 	private SpriteRenderer rend;
 
+
+	private bool isGrounded;
 	private Vector2 playerDirection;
 
 	void Start () {
@@ -22,7 +25,11 @@ public class PlayerController : MonoBehaviour {
 
 	void FixedUpdate() {
 		float horizontal = Input.GetAxisRaw ("Horizontal");
-		float vertical = Input.GetAxisRaw ("Jump");
+		float vertical = 0;
+
+		if (isGrounded) {
+			vertical = Input.GetAxisRaw ("Jump") * jumpForce;
+		}
 
 		if ( horizontal != 0 || vertical != 0) {
 
@@ -32,5 +39,19 @@ public class PlayerController : MonoBehaviour {
 			rb.AddForce (playerDirection * playerSpeed);
 		}
 
+	}
+		
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Platform") {
+			isGrounded = true;
+			Debug.Log ("Hello");
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D coll)
+	{
+		if(coll.gameObject.tag == "Platform")
+			isGrounded=false;
+			Debug.Log ("Bye");
 	}
 }
