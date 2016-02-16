fc323cb107b83d429daf78091f10db574fdc7869
Vitaly <akaredsc2@gmail.com>
2016-02-16 21:52:55 +0200

    added platforms and player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float playerSpeed;
+
+	private Rigidbody2D rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+		
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		float horizontal = Input.GetAxisRaw ("Horizontal");
+
+		rb.AddForce (Vector2.right * horizontal * playerSpeed);
+	}
+}
